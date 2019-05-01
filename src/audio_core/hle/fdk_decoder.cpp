c71d6939870643ff9f62092c404044a38f6d76ca
liushuyu <liushuyu011@gmail.com>
2019-04-30 19:07:53 -0600

    audio_core: dsp_hle: implements fdk_aac decoder

---
+++ src/audio_core/hle/fdk_decoder.cpp
@@ -0,0 +1,178 @@
+// Copyright 2019 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include "audio_core/hle/fdk_decoder.h"
+
+namespace AudioCore::HLE {
+
+class FDKDecoder::Impl {
+public:
+    explicit Impl(Memory::MemorySystem& memory);
+    ~Impl();
+    std::optional<BinaryResponse> ProcessRequest(const BinaryRequest& request);
+
+private:
+    std::optional<BinaryResponse> Initalize(const BinaryRequest& request);
+
+    std::optional<BinaryResponse> Decode(const BinaryRequest& request);
+
+    Memory::MemorySystem& memory;
+
+    HANDLE_AACDECODER decoder = NULL;
+};
+
+FDKDecoder::Impl::Impl(Memory::MemorySystem& memory) : memory(memory) {
+    // choose the input format when initializing: 1 layer of ADTS
+    decoder = aacDecoder_Open(TRANSPORT_TYPE::TT_MP4_ADTS, 1);
+}
+
+std::optional<BinaryResponse> FDKDecoder::Impl::Initalize(const BinaryRequest& request) {
+    BinaryResponse response;
+    std::memcpy(&response, &request, sizeof(response));
+    response.unknown1 = 0x0;
+    // TODO: identify the broken fdk_aac implementation and refuse to initialize
+    // once identified as broken (check for module IDs?)
+    // LIB_INFO decoder_info = {};
+
+    if (decoder) {
+        LOG_INFO(Audio_DSP, "FDK Decoder initialized");
+    } else {
+        LOG_ERROR(Audio_DSP, "Decoder not initialized");
+    }
+
+    return response;
+}
+
+FDKDecoder::Impl::~Impl() {
+    aacDecoder_Close(decoder);
+}
+
+std::optional<BinaryResponse> FDKDecoder::Impl::ProcessRequest(const BinaryRequest& request) {
+    if (request.codec != DecoderCodec::AAC) {
+        LOG_ERROR(Audio_DSP, "FDK AAC Decoder cannot handle such codec: {}",
+                  static_cast<u16>(request.codec));
+        return {};
+    }
+
+    switch (request.cmd) {
+    case DecoderCommand::Init: {
+        return Initalize(request);
+    }
+    case DecoderCommand::Decode: {
+        return Decode(request);
+    }
+    case DecoderCommand::Unknown: {
+        BinaryResponse response;
+        std::memcpy(&response, &request, sizeof(response));
+        response.unknown1 = 0x0;
+        return response;
+    }
+    default:
+        LOG_ERROR(Audio_DSP, "Got unknown binary request: {}", static_cast<u16>(request.cmd));
+        return {};
+    }
+}
+
+std::optional<BinaryResponse> FDKDecoder::Impl::Decode(const BinaryRequest& request) {
+    BinaryResponse response;
+    response.codec = request.codec;
+    response.cmd = request.cmd;
+    response.size = request.size;
+
+    if (!decoder) {
+        LOG_DEBUG(Audio_DSP, "Decoder not initalized");
+        // This is a hack to continue games that are not compiled with the aac codec
+        response.num_channels = 2;
+        response.num_samples = 1024;
+        return response;
+    }
+
+    if (request.src_addr < Memory::FCRAM_PADDR ||
+        request.src_addr + request.size > Memory::FCRAM_PADDR + Memory::FCRAM_SIZE) {
+        LOG_ERROR(Audio_DSP, "Got out of bounds src_addr {:08x}", request.src_addr);
+        return {};
+    }
+    u8* data = memory.GetFCRAMPointer(request.src_addr - Memory::FCRAM_PADDR);
+
+    std::array<std::vector<s16>, 2> out_streams;
+
+    std::size_t data_size = request.size;
+
+    // decoding loops
+    AAC_DECODER_ERROR result = AAC_DEC_OK;
+    // 8192 units of s16 are enough to hold one frame of AAC-LC or AAC-HE/v2 data
+    s16 decoder_output[8192];
+    // note that we don't free this pointer as it is automatically freed by fdk_aac
+    CStreamInfo* stream_info;
+    // how many bytes to be queued into the decoder, decrement from the buffer size
+    u32 buffer_remaining = data_size;
+    // alias the data_size as an u32
+    u32 input_size = data_size;
+
+    while (buffer_remaining) {
+        // queue the input buffer
+        result = aacDecoder_Fill(decoder, &data, &input_size, &buffer_remaining);
+        if (result != AAC_DEC_OK) {
+            // there are some issues when queuing the input buffer
+            LOG_ERROR(Audio_DSP, "Failed to enqueue the input samples");
+            return std::nullopt;
+        }
+        // get output from decoder
+        result = aacDecoder_DecodeFrame(decoder, decoder_output, 8192, 0);
+        if (result == AAC_DEC_OK) {
+            // get the stream information
+            stream_info = aacDecoder_GetStreamInfo(decoder);
+            // fill the stream information for binary response
+            response.num_channels = stream_info->aacNumChannels;
+            response.num_samples = stream_info->frameSize;
+            // fill the output
+            // the sample size = frame_size * channel_counts
+            for (int sample = 0; sample < (stream_info->frameSize * 2); sample++) {
+                for (int ch = 0; ch < stream_info->aacNumChannels; ch++) {
+                    out_streams[ch].push_back(decoder_output[(sample * 2) + 1]);
+                }
+            }
+        } else if (result == AAC_DEC_TRANSPORT_SYNC_ERROR) {
+            // decoder has some synchronization problems, try again with new samples
+            // using old samples might trigger this error again
+            continue;
+        } else {
+            LOG_ERROR(Audio_DSP, "Error decoding the sample: {}", result);
+            return std::nullopt;
+        }
+    }
+    // transfer the decoded buffer from vector to the FCRAM
+    if (out_streams[0].size() != 0) {
+        if (request.dst_addr_ch0 < Memory::FCRAM_PADDR ||
+            request.dst_addr_ch0 + out_streams[0].size() >
+                Memory::FCRAM_PADDR + Memory::FCRAM_SIZE) {
+            LOG_ERROR(Audio_DSP, "Got out of bounds dst_addr_ch0 {:08x}", request.dst_addr_ch0);
+            return {};
+        }
+        std::memcpy(memory.GetFCRAMPointer(request.dst_addr_ch0 - Memory::FCRAM_PADDR),
+                    out_streams[0].data(), out_streams[0].size());
+    }
+
+    if (out_streams[1].size() != 0) {
+        if (request.dst_addr_ch1 < Memory::FCRAM_PADDR ||
+            request.dst_addr_ch1 + out_streams[1].size() >
+                Memory::FCRAM_PADDR + Memory::FCRAM_SIZE) {
+            LOG_ERROR(Audio_DSP, "Got out of bounds dst_addr_ch1 {:08x}", request.dst_addr_ch1);
+            return {};
+        }
+        std::memcpy(memory.GetFCRAMPointer(request.dst_addr_ch1 - Memory::FCRAM_PADDR),
+                    out_streams[1].data(), out_streams[1].size());
+    }
+    return response;
+}
+
+FDKDecoder::FDKDecoder(Memory::MemorySystem& memory) : impl(std::make_unique<Impl>(memory)) {}
+
+FDKDecoder::~FDKDecoder() = default;
+
+std::optional<BinaryResponse> FDKDecoder::ProcessRequest(const BinaryRequest& request) {
+    return impl->ProcessRequest(request);
+}
+
+} // namespace AudioCore::HLE
